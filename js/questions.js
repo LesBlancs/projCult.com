18905941cde96d16fc4de3ac380ae32b64125698
Janes <46504821+janes-r@users.noreply.github.com>
2021-01-22 10:10:51 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -4,79 +4,126 @@ function questions(numero)
 	switch(numero)
 	{
 		case 1:
-		chaine=" *Vous vous sentez dynamique*Vous vous sentez calme*2";		break;
+		chaine=" *Vous vous sentez dynamique*Vous vous sentez calme*2";
+		break;
 		case 2:
-		chaine=" *Vous aimez parler*Vous aimez  \351couter*2";		break;
+		chaine=" *Vous aimez parler*Vous aimez  \351couter*2";
+		break;
 		case 3:
-		chaine=" *Vous penser \340 haute voix*Vous penser dans votre t\352te*2";		break;
+		chaine=" *Vous penser \340 haute voix*Vous penser dans votre t\352te*2";
+		break;
 		case 4:
-		chaine=" *Vous agissez,puis pensez*Vous pensz,puis agissez*2";		break;
+		chaine=" *Vous agissez,puis pensez*Vous pensez,puis agissez*2";
+		break;
 		case 5:
-		chaine=" *Vous n\047aimez pas etre seul*Vous vous sentez bien quand vous \352tes seul*2";		break;
+		chaine=" *Vous n\047aimez pas etre seul*Vous vous sentez bien quand vous \352tes seul*2";
+		break;
 		case 6:
-		chaine=" *Vous aimez  \351tablir de nouveaux contacts*Vous aimez approfondir vos contacts*2";		break;
+		chaine=" *Vous aimez  \351tablir de nouveaux contacts*Vous aimez approfondir vos contacts*2";
+		break;
 		case 7:
-		chaine=" *Vous pr \351f \351rez parler plut\364t qu\047\351crire*Vous \352tes consid\351r\351 comme plut\364t secret et r\351serv\351*2";		break;
+		chaine=" *Vous pr \351f \351rez parler plut\364t qu\047\351crire*Vous \352tes consid\351r\351 comme plut\364t secret et r\351serv\351*2";
+		break;
 		case 8:
-		chaine=" *Vous pouvez facilement \352tre distrait*Vous poss\351dez une bonne capacit\351 de concentration*2";		break;
+		chaine=" *Vous pouvez facilement \352tre distrait*Vous poss\351dez une bonne capacit\351 de concentration*2";
+		break;
 		case 9:
-		chaine=" *Vous pr\351f\351rez faire plusieurs choses \340 la fois*Vous pr\351f\351rez vous concentrer sur une seule chose \340 la fois*2";		break;
+		chaine=" *Vous pr\351f\351rez faire plusieurs choses \340 la fois*Vous pr\351f\351rez vous concentrer sur une seule chose \340 la fois*2";
+		break;
 		case 10:
-		chaine=" *Vous avez parfois un discours changeant*Vous \352tes ind\351pendant*2";		break;
+		chaine=" *Vous avez parfois un discours changeant*Vous \352tes ind\351pendant*2";
+		break;
 		case 11:
-		chaine=" *Vous vous attachez aux faits et aux d\351tails*Vous vous int\351ressez aux id\351es*2";		break;
+		chaine=" *Vous vous attachez aux faits et aux d\351tails*Vous vous int\351ressez aux id\351es*2";
+		break;
 		case 12:
-		chaine=" *Vous aimez les choses utiles*Vous remarquez tout ce qui est nouveau et diff\351rent*2";		break;
+		chaine=" *Vous aimez les choses utiles*Vous remarquez tout ce qui est nouveau et diff\351rent*2";
+		break;
 		case 13:
-		chaine=" *Vous vivez dans l\047instant pr\351sent*Vous pensez aux implications futures*2";		break;
+		chaine=" *Vous vivez dans l\047instant pr\351sent*Vous pensez aux implications futures*2";
+		break;
 		case 14:
-		chaine=" *Vous faites confiance \340 l\047exp\351rience*Vous suivez votre instinct*2";		break;
+		chaine=" *Vous faites confiance \340 l\047exp\351rience*Vous suivez votre instinct*2";
+		break;
 		case 15:
-		chaine=" *Vous aimez approfondir vos comp\351tences*Vous aimez apprendre de nouvelles comp\351tences*2";		break;
+		chaine=" *Vous aimez approfondir vos comp\351tences*Vous aimez apprendre de nouvelles comp\351tences*2";
+		break;
 		
 		case 16:
-		chaine=" *Vous restez fid\350le aux m\351thodes qui ont fait leurs preuves*Vous n\047aimez pas la routine*2";		break;
+		chaine=" *Vous restez fid\350le aux m\351thodes qui ont fait leurs preuves*Vous n\047aimez pas la routine*2";
+		break;
 		case 17:
-		chaine=" *Vous pr\351f\351rez les instructions \351tape par \351tape*Vous cherchez \340 comprendre*2";		break;
+		chaine=" *Vous pr\351f\351rez les instructions \351tape par \351tape*Vous cherchez \340 comprendre*2";
+		break;
 		case 18:
-		chaine=" *Vous \352tes pratique*Vous \352tes th \351orique*2";		break;
+		chaine=" *Vous \352tes pratique*Vous \352tes th \351orique*2";
+		break;
 		case 19:
-		chaine=" *Vous aimez ce qui est concret, r\351el, directement observable*Vous \352tes attir\351s par les id\351es originales*2";		break;
+		chaine=" *Vous aimez ce qui est concret, r\351el, directement observable*Vous \352tes attir\351s par les id\351es originales*2";
+		break;
 		case 20:
-		chaine=" *Vous \352tes r\351aliste : vous voyez ce qui existe*Vous \352tes imaginatifs : vous voyez les possibilit\351s*2";		break;
+		chaine=" *Vous \352tes r\351aliste : vous voyez ce qui existe*Vous \352tes imaginatifs : vous voyez les possibilit\351s*2";
+		break;
 		case 21:
-		chaine=" *Vous vous efforcez d\047\352tre objectif dans vos d\351cisions*Vous fondez vos d\351cisions sur vos valeurs et vos sentiments*2";		break;
+		chaine=" *Vous vous efforcez d\047\352tre objectif dans vos d\351cisions*Vous fondez vos d\351cisions sur vos valeurs et vos sentiments*2";
+		break;
 		case 22:
-		chaine=" *Vous apparaissez calme et r\351serv\351*Vous \352tes sociable et amical*2";		break;
+		chaine=" *Vous apparaissez calme et r\351serv\351*Vous \352tes sociable et amical*2";
+		break;
 		case 23:
-		chaine=" *Vous avez un sens aigu de la justice*Vous avez tendance \340 la cl\351mence*2";		break;
+		chaine=" *Vous avez un sens aigu de la justice*Vous avez tendance \340 la cl\351mence*2";
+		break;
 		case 24:
-		chaine=" *Vous vous impliquez peu, vous prenez de la distance*Vous prenez les choses \340 coeur*2";		break;
+		chaine=" *Vous vous impliquez peu, vous prenez de la distance*Vous prenez les choses \340 coeur*2";
+		break;
 		case 25:
-		chaine=" *Vous \352tes critique (vous remarquez vite les failles et les d\351fauts)*Vous tentez de faire plaisir (prompt \340 faire des compliments)*2";		break;
+		chaine=" *Vous \352tes critique (vous remarquez vite les failles et les d\351fauts)*Vous tentez de faire plaisir (prompt \340 faire des compliments)*2";
+		break;
 		case 26:
-		chaine=" *Vous adorez argumenter pour le plaisir*Vous \351vitez la discussion et le conflit*2";		break;
+		chaine=" *Vous adorez argumenter pour le plaisir*Vous \351vitez la discussion et le conflit*2";
+		break;
 		case 27:
-		chaine=" *Vous \352tes franc et direct*Vous \352tes diplomate et fa\356tes preuve de tact*2";		break;
+		chaine=" *Vous \352tes franc et direct*Vous \352tes diplomate et fa\356tes preuve de tact*2";
+		break;
 		case 28:
-		chaine=" *Vous \352tes motiv\351 par vos projets*Vous \352tes motiv\351 par l\047estime des autres*2";		break;
+		chaine=" *Vous \352tes motiv\351 par vos projets*Vous \352tes motiv\351 par l\047estime des autres*2";
+		break;
 		case 29:
-		chaine=" *Vous aimez vous placer en observateur*Vous \352tes sensible (facilement bless\351)*2";		break;
+		chaine=" *Vous aimez vous placer en observateur*Vous \352tes sensible (facilement bless\351)*2";
+		break;
 		case 30:
-		chaine=" *Vous \352tes sensible \340 la logique*Vous fa\356tes confiance \340 vos impressions*2";		break;
+		chaine=" *Vous \352tes sensible \340 la logique*Vous fa\356tes confiance \340 vos impressions*2";
+		break;
 		case 31:
-		chaine=" *Vous aimez organiser et planifier*Vous aimez vivre de fa\347on flexible*2";		break;
+		chaine=" *Vous aimez organiser et planifier*Vous aimez vivre de fa\347on flexible*2";
+		break;
 		case 32:
-		chaine=" *Vous \352tes s\351rieux et conventionnel*Vous  \352tes ludique et non-conventionnel*2";		break;
+		chaine=" *Vous \352tes s\351rieux et conventionnel*Vous  \352tes ludique et non-conventionnel*2";
+		break;
 		case 33:
-		chaine=" *Vous suivez votre calendrier et  \352tes parfaitement ponctuel*Vous n\047avez ni heure ni d\351lais*2";		break;
-		case 34:		chaine=" *Vous aimez terminer vos projets*Vous aimez d\351marrer des projets*2";		break;
-		case 35:		chaine=" *Vous travaillez d\047abord, vous vous amusez ensuite*Vous vous amusez d\047abord et travaillez ensuite*2";		break;
-		case 36:		chaine=" *Vous n\047aimez pas le stress de derni\350re minute*Vous rechignez \340 vous engager*2";		break;
-		case 37:		chaine=" *Vous ne discutez pas les r\350gles*Vous discutez les r\350gles*2";		break;
-		case 38:		chaine=" *Vous cherchez \340 ma\356triser*Vous cherchez \340 comprendre*2";		break;
-		case 39:		chaine=" *Vous  \352tes \340 l\047aise au sein de structures bien d\351finies*Vous aimez conserver votre libert\351 d'action*2";		break;
-		case 40:		chaine=" *Vous n\047aimez pas le provisoire, l\047incertain*Vous restez ouvert, aimez vivre des exp\351riences, vous adapter*2";		break;
+		chaine=" *Vous suivez votre calendrier et  \352tes parfaitement ponctuel*Vous n\047avez ni heure ni d\351lais*2";
+		break;
+		case 34:
+		chaine=" *Vous aimez terminer vos projets*Vous aimez d\351marrer des projets*2";
+		break;
+		case 35:
+		chaine=" *Vous travaillez d\047abord, vous vous amusez ensuite*Vous vous amusez d\047abord et travaillez ensuite*2";
+		break;
+		case 36:
+		chaine=" *Vous n\047aimez pas le stress de derni\350re minute*Vous rechignez \340 vous engager*2";
+		break;
+		case 37:
+		chaine=" *Vous ne discutez pas les r\350gles*Vous discutez les r\350gles*2";
+		break;
+		case 38:
+		chaine=" *Vous cherchez \340 ma\356triser*Vous cherchez \340 comprendre*2";
+		break;
+		case 39:
+		chaine=" *Vous  \352tes \340 l\047aise au sein de structures bien d\351finies*Vous aimez conserver votre libert\351 d'action*2";
+		break;
+		case 40:
+		chaine=" *Vous n\047aimez pas le provisoire, l\047incertain*Vous restez ouvert, aimez vivre des exp\351riences, vous adapter*2";
+		break;
 		
 	}
 	
