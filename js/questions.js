eaba3d7fe8f3a2c1fb423f777b6e741ccaeb138c
Janes <46504821+janes-r@users.noreply.github.com>
2021-01-22 11:12:05 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -4,125 +4,125 @@ function questions(numero)
 	switch(numero)
 	{
 		case 1:
-		chaine=" *Vous vous sentez dynamique*Vous vous sentez calme*2";
+		chaine="vous etes plus du genre :*dynamique *calme*2";
 		break;
 		case 2:
-		chaine=" *Vous aimez parler*Vous aimez  \351couter*2";
+		chaine="Dans une conversation,vous preferez :*etre a la tete de la discussion *ecouter/etre auditeur*2";
 		break;
 		case 3:
-		chaine=" *Vous pensez \340 haute voix*Vous pensez dans votre t\352te*2";
+		chaine="Vous etes du style a :*penser a haute voix *penser dans votre tete*2";
 		break;
 		case 4:
-		chaine=" *Vous agissez,puis pensez*Vous pensez,puis agissez*2";
+		chaine="Vous agissez d'abord avant de penser ?*Ouais,toujours ou tres souvent *Non,jamais ou peu souvent*2";
 		break;
 		case 5:
-		chaine=" *Vous n\047aimez pas etre seul*Vous vous sentez bien quand vous \352tes seul*2";
+		chaine=" Ce qui vous preferez le plus est :*La solitude *Le groupe*2";
 		break;
 		case 6:
-		chaine=" *Vous aimez  \351tablir de nouveaux contacts*Vous aimez approfondir vos contacts*2";
+		chaine="Pour vous, le mieux est de : *créer de nouveaux contacts *approfondir vos contacts déjà crées*2";
 		break;
 		case 7:
-		chaine=" *Vous pr\351f\351rez parler plut\364t qu\047\351crire*Vous \352tes consid\351r\351 comme plut\364t secret et r\351serv\351*2";
+		chaine="Ce qui vous caractérise le mieux est : *Parler plutôt que écrire *réservé et avec beaucoup de secrets*2";
 		break;
 		case 8:
-		chaine=" *Vous pouvez facilement \352tre distrait*Vous poss\351dez une bonne capacit\351 de concentration*2";
+		chaine="Vous êtes plus du genre à : *être facilement distrait *posséder une bonne capacite de concentration*2";
 		break;
 		case 9:
-		chaine=" *Vous pr\351f\351rez faire plusieurs choses \340 la fois*Vous pr\351f\351rez vous concentrer sur une seule chose \340 la fois*2";
+		chaine=" *: Pendant un travail de groupe : *vous préférez faire plusieurs choses à la fois *Vous préférez vous concentrer sur une seule chose à la fois*2";
 		break;
 		case 10:
-		chaine=" *Vous avez parfois un discours changeant*Vous \352tes ind\351pendant*2";
+		chaine="Vous êtes plus du genre à : *avoir parfois un discours changeant *être indépendant*2";
 		break;
 		case 11:
-		chaine=" *Vous vous attachez aux faits et aux d\351tails*Vous vous int\351ressez aux id\351es*2";
+		chaine="Vous êtes beaucoup plus amené à vous : *attacher aux faits et aux détails (réalité) *intéresser a des idées (imagination)*2";
 		break;
 		case 12:
-		chaine=" *Vous aimez les choses utiles*Vous remarquez tout ce qui est nouveau et diff\351rent*2";
+		chaine="Votre personnalité vous pousse à : *aimer les choses utiles *remarquer tout ce qui est nouveau et différent*2";
 		break;
 		case 13:
-		chaine=" *Vous vivez dans l\047instant pr\351sent*Vous pensez aux implications futures*2";
+		chaine="Il vous arrive de vous poser des questions sur le futur : *Oui, ça m'arrive *Non, pas tout le temps*2";
 		break;
 		case 14:
-		chaine=" *Vous faites confiance \340 l\047exp\351rience*Vous suivez votre instinct*2";
+		chaine="Pour vous, le plus important c'est :*l'expérience *votre instinct*2";
 		break;
 		case 15:
-		chaine=" *Vous aimez approfondir vos comp\351tences*Vous aimez apprendre de nouvelles comp\351tences*2";
+		chaine="Vous aimez plutôt : *approfondir vos compétences *apprendre de nouvelles compétences*2";
 		break;
 		
 		case 16:
-		chaine=" *Vous restez fid\350le aux m\351thodes qui ont fait leurs preuves*Vous n\047aimez pas la routine*2";
+		chaine="Vous n'avez pas de soucis à tenter de nouvelles expériences : *Oui, c'est tout moi ça *Non, ça peut être risqué parfois*2";
 		break;
 		case 17:
-		chaine=" *Vous pr\351f\351rez les instructions \351tape par \351tape*Vous cherchez \340 comprendre*2";
+		chaine="Vous êtes du genre à être organisé pendant un travail : *Toujours *pas vraiment*2";
 		break;
 		case 18:
-		chaine=" *Vous \352tes pratique*Vous \352tes th\351orique*2";
+		chaine="Pour vous la pratique passe avant la théorie : * ça a fait ses preuves *ça peut être dangereux*2";
 		break;
 		case 19:
-		chaine=" *Vous aimez ce qui est concret, r\351el, directement observable*Vous \352tes attir\351 par les id\351es originales*2";
+		chaine="Vous êtes plus du genre à donner : *des preuves concrètes *des hypothèses bien établies*2";
 		break;
 		case 20:
-		chaine=" *Vous \352tes r\351aliste : vous voyez ce qui existe*Vous \352tes imaginatif : vous voyez les possibilit\351s*2";
+		chaine="Vous êtes beaucoup plus : *réaliste *imaginatif*2";
 		break;
 		case 21:
-		chaine=" *Vous vous efforcez d\047\352tre objectif dans vos d\351cisions*Vous fondez vos d\351cisions sur vos valeurs et vos sentiments*2";
+		chaine="Vous revenez rarement sur vos décisions : *Oui, je suis d'accord *Non, pas toujours *2";
 		break;
 		case 22:
-		chaine=" *Vous apparaissez calme et r\351serv\351*Vous \352tes sociable et amical*2";
+		chaine="vous pouvez vous qualifier de : *calme et réservé *sociable et amicale*2";
 		break;
 		case 23:
-		chaine=" *Vous avez un sens aigu de la justice*Vous avez tendance \340 la cl\351mence*2";
+		chaine="Vous êtes plus amené à avoir de l'empathie même dans des situations graves : *Oui, ça m'arrive parfois *Alors là jamais*2";
 		break;
 		case 24:
-		chaine=" *Vous vous impliquez peu, vous prenez de la distance*Vous prenez les choses \340 coeur*2";
+		chaine="Pendant un travail de groupe : *Vous vous impliquez peu, vous prenez de la distance *Vous prenez les choses à cœur*2";
 		break;
 		case 25:
-		chaine=" *Vous \352tes critique (vous remarquez vite les failles et les d\351fauts)*Vous tentez de faire plaisir (prompt \340 faire des compliments)*2";
+		chaine="Une des choses qui vous caractérise le plus c'est : *votre honnêteté (vous n'hésitez pas à dire la vérité même ci ça peut faire très mal aux autres) *Votre gentillesse (vous préférez toujours faire plaisir aux autres même ci il faut mentir)*2";
 		break;
 		case 26:
-		chaine=" *Vous adorez argumenter pour le plaisir*Vous \351vitez la discussion et le conflit*2";
+		chaine="Vous êtes du genre a : *argumenter pour le plaisir *évitez la discussion et le débat*2";
 		break;
 		case 27:
-		chaine=" *Vous \352tes franc et direct*Vous \352tes diplomate et fa\356tes preuve de tact*2";
+		chaine="Vous vous trouvez : *franc et direct *diplomate et avec beaucoup de tact*2";
 		break;
 		case 28:
-		chaine=" *Vous \352tes motiv\351 par vos projets*Vous \352tes motiv\351 par l\047estime des autres*2";
+		chaine="Le plus important c'est : *Avoir des objectifs *être respecte par les autres*2";
 		break;
 		case 29:
-		chaine=" *Vous aimez vous placer en observateur*Vous \352tes sensible (facilement bless\351)*2";
+		chaine="Vous êtes plus du style a : *Prendre beaucoup de recul sur les choses *être contrôle par ses émotions (vous êtes très sensible)*2";
 		break;
 		case 30:
-		chaine=" *Vous \352tes sensible \340 la logique*Vous fa\356tes confiance \340 vos impressions*2";
+		chaine="Ce qui importe dans toute chose c'est la logique : *C'est ma règle de vie *je ne suis pas d'accord*2";
 		break;
 		case 31:
-		chaine=" *Vous aimez organiser et planifier*Vous aimez vivre de fa\347on flexible*2";
+		chaine="Pour vous, être organisé(e) est plus important qu’être flexible. *Je suis tout à fait d'accord * Je ne suis pas du tout d'accord*2";
 		break;
 		case 32:
-		chaine=" *Vous \352tes s\351rieux et conventionnel*Vous  \352tes ludique et non-conventionnel*2";
+		chaine="vous direz que vous êtes : *sérieux et conventionnel *ludique et non-conventionnel*2";
 		break;
 		case 33:
-		chaine=" *Vous suivez votre calendrier et  \352tes parfaitement ponctuel*Vous n\047avez ni heure ni d\351lais*2";
+		chaine="La ponctualité fait partie de vos qualités : *Ouais, bien évidemment *Non, pas du tout*2";
 		break;
 		case 34:
-		chaine=" *Vous aimez terminer vos projets*Vous aimez d\351marrer des projets*2";
+		chaine="Vous êtes du genre a toujours : *terminer un projet avant d'enchainer *accumuler des projets*2";
 		break;
 		case 35:
-		chaine=" *Vous travaillez d\047abord, vous vous amusez ensuite*Vous vous amusez d\047abord et travaillez ensuite*2";
+		chaine="Vous placez le travail en priorité et le divertissement à la fin de celui-ci*Oui, toujours *Non, des pauses ne font pas de mal*2";
 		break;
 		case 36:
-		chaine=" *Vous n\047aimez pas le stress de derni\350re minute*Vous rechignez \340 vous engager*2";
+		chaine="L'imprévu ne vous effraie pas : *Je suis très flexible *ça m'effraie beaucoup*2";
 		break;
 		case 37:
-		chaine=" *Vous ne discutez pas les r\350gles*Vous discutez les r\350gles*2";
+		chaine="Vous êtes du style rebelle : *Je suis d'accord *Je suis loin de l'être*2";
 		break;
 		case 38:
-		chaine=" *Vous cherchez \340 ma\356triser*Vous cherchez \340 comprendre*2";
+		chaine="Ce qui vous importe le plus c'est : *comprendre entièrement le sujet *comprendre un minimum le sujet*2";
 		break;
 		case 39:
-		chaine=" *Vous  \352tes \340 l\047aise au sein de structures bien d\351finies*Vous aimez conserver votre libert\351 d'action*2";
+		chaine="Vous vous trouvez plus du genre a : *Préférer des structures bien définies (avoir des supérieurs) *Préférer votre liberté d'action (Ne recevoir d'ordre de personne)*2";
 		break;
 		case 40:
-		chaine=" *Vous n\047aimez pas le provisoire, l\047incertain*Vous restez ouvert, aimez vivre des exp\351riences, vous adapter*2";
+		chaine="Vous êtes du style à préférer : *de nouvelles expériences *ce que vous maitrisez déjà ou vous êtes à l'aise.*2";
 		break;
 		
 	}
