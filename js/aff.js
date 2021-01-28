f06276e2e3a215739277b878a06d3108039b9755
Janes <46504821+janes-r@users.noreply.github.com>
2021-01-28 15:50:24 +0100

    Update aff.js

--- js/aff.js
+++ js/aff.js
@@ -1,5 +1,5 @@
 
-/*
+
 var temp ="";
 
 var temp2 = "";
@@ -9,9 +9,9 @@ var categorie = [0,0,0,0];
 var config1;
 var config2;
 
-*/
 
 
+/*
 const {google} = require('googleapis');
 const keys = require('keys.json');
 
@@ -45,11 +45,105 @@ async function gsrun(cl){
 
 
 
+*/
+
+
+    function makeApiCall() {
+      var params = {
+        // The ID of the spreadsheet to retrieve data from.
+        spreadsheetId: '1iPU6hnVEWl2TCNVz2Zk3Fyq331HmzZlpM6o-9bBtsOo',  // TODO: Update placeholder value.
+
+        // The A1 notation of the values to retrieve.
+        range: 'Réponses au formulaire 3',  // TODO: Update placeholder value.
+
+        // How values should be represented in the output.
+        // The default render option is ValueRenderOption.FORMATTED_VALUE.
+        valueRenderOption: 'FORMATTED_VALUE',  // TODO: Update placeholder value.
+
+        // How dates, times, and durations should be represented in the output.
+        // This is ignored if value_render_option is
+        // FORMATTED_VALUE.
+        // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+        dateTimeRenderOption: 'SERIAL_NUMBER',  // TODO: Update placeholder value.
+      };
+
+      var request = gapi.client.sheets.spreadsheets.values.get(params);
+      request.then(function(response) {
+        // TODO: Change code below to process the `response` object:
+        $("#data").empty();
+        //$("#data").html(response.result.values);
+        temp = response.result.values;
+	data2 = lanc();
+        //alert("tp1 "+data2);
+	      
+	let nb =data2[0]+data2[1]+data2[2]+data2[3];
+	document.getElementById('nbPersonne').innerText = "nombre de personne ayant répondu au atyTest : "+nb;
+	
+	 myPie1.update();
+	 myPie2.update();    
+         console.log(response.result.values);
+      }, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+      });
+    }
+
+    function initClient() {
+      var API_KEY = 'AIzaSyAz6TlBAZrHtJ6f9UR9mdRMXV_wifWrINY';  // TODO: Update placeholder with desired API key.
+
+      var CLIENT_ID = '1021725875690-r08u3n6aclvuog72ud8fqrn1rvm0f6hh.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
+
+      // TODO: Authorize using one of the following scopes:
+      //   'https://www.googleapis.com/auth/drive'
+      //   'https://www.googleapis.com/auth/drive.file'
+      //   'https://www.googleapis.com/auth/drive.readonly'
+      //   'https://www.googleapis.com/auth/spreadsheets'
+      //   'https://www.googleapis.com/auth/spreadsheets.readonly'
+      var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+	    
+	    /*
+	    gapi.client.init({
+        'apiKey': '3379d05a4cacb1890cc85fef4a9ca4dce676db7f',
+        'clientId': '100855812677045796937',
+        'scope': SCOPE,
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+      }).then(function() {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      });*/
+
+      gapi.client.init({
+        'apiKey': API_KEY,
+        'clientId': CLIENT_ID,
+        'scope': SCOPE,
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+      }).then(function() {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      });
+      
+    }
+
+    function handleClientLoad() {
+      gapi.load('client:auth2', initClient);
+    }
+
+    function updateSignInStatus(isSignedIn) {
+      if (isSignedIn) {
+        makeApiCall();
+      }
+    }
+
+    function handleSignInClick(event) {
+      gapi.auth2.getAuthInstance().signIn();
+    }
+
+    function handleSignOutClick(event) {
+      gapi.auth2.getAuthInstance().signOut();
+    }
+    
 
 
 
-
-/*
 function lanc() {
 	
 //alert("tp2: "+temp);
@@ -214,6 +308,6 @@ window.onload = function() {
         	window.myPie2 = new Chart(ctx, config2);
     		};
 
-*/
+
 	
    
