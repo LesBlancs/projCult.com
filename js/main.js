3d7c8500e380d0a82e957e9722bfcb3a30b5f7f9
Janes <46504821+janes-r@users.noreply.github.com>
2021-01-28 14:31:52 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -153,7 +153,7 @@
 			sessionStorage.setItem("data",temp);
 			document.getElementById('result').removeAttribute('disabled');
 			document.getElementById('result').removeAttribute('hidden');
-			window.location.href = "contact.html";
+			window.location.href = "result.html";
 			resultat();
 			//document.cookie = "data = "+e+ " "+i+ " "+s+ " "+n+ " "+t+ " "+f+ " "+j+ " "+p+"";
 			//document.cookie =	"username=John Smith; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
