36d5b4b9596083e2305bb6f9d5e6da8de2d59142
Janes <46504821+janes-r@users.noreply.github.com>
2021-01-28 01:57:59 +0100

    Update aff.js

--- js/aff.js
+++ js/aff.js
@@ -240,4 +240,12 @@ var config2 = {
 };
 
 
-
+	
+    window.onload = function() {
+        var ctx = document.getElementById('chart-area').getContext('2d');
+		
+        window.myPie = new Chart(ctx, config1);
+		var ctx = document.getElementById('chart-area2').getContext('2d');
+		
+        window.myPie = new Chart(ctx, config2);
+    };
