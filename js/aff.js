c6d6bbba0573d13112ec6c10ab68ccf26d25c01a
Janes <46504821+janes-r@users.noreply.github.com>
2021-01-28 13:39:14 +0100

    Update aff.js

--- js/aff.js
+++ js/aff.js
@@ -10,107 +10,39 @@ var config2;
 
 
 
+cons {google} = require('googleapis');
+const keys = require('./keys.json');
 
+const client = new google.auth.JWT(
+	keys.client_email, 
+	null, 
+	key.private_key,
+	['https://www.googleapis.com/auth/spreadsheets']
+);
 
-
-
-
-
-
-
-    function makeApiCall() {
-      var params = {
-        // The ID of the spreadsheet to retrieve data from.
-        spreadsheetId: '1iPU6hnVEWl2TCNVz2Zk3Fyq331HmzZlpM6o-9bBtsOo',  // TODO: Update placeholder value.
-
-        // The A1 notation of the values to retrieve.
-        range: 'Réponses au formulaire 3',  // TODO: Update placeholder value.
-
-        // How values should be represented in the output.
-        // The default render option is ValueRenderOption.FORMATTED_VALUE.
-        valueRenderOption: 'FORMATTED_VALUE',  // TODO: Update placeholder value.
-
-        // How dates, times, and durations should be represented in the output.
-        // This is ignored if value_render_option is
-        // FORMATTED_VALUE.
-        // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-        dateTimeRenderOption: 'SERIAL_NUMBER',  // TODO: Update placeholder value.
-      };
-
-      var request = gapi.client.sheets.spreadsheets.values.get(params);
-      request.then(function(response) {
-        // TODO: Change code below to process the `response` object:
-        $("#data").empty();
-        //$("#data").html(response.result.values);
-        temp = response.result.values;
-	data2 = lanc();
-        //alert("tp1 "+data2);
-	      
-	let nb =data2[0]+data2[1]+data2[2]+data2[3];
-	document.getElementById('nbPersonne').innerText = "nombre de personne ayant répondu au atyTest : "+nb;
+client.authorize(function(err,tokens){
 	
-	 myPie1.update();
-	 myPie2.update();    
-         console.log(response.result.values);
-      }, function(reason) {
-        console.error('error: ' + reason.result.error.message);
-      });
-    }
+	if(err){
+		console.log(err);
+		return;
+	}else {
+		console.log('Connected!');	
+	}
+});
 
-    function initClient() {
-      var API_KEY = 'AIzaSyAz6TlBAZrHtJ6f9UR9mdRMXV_wifWrINY';  // TODO: Update placeholder with desired API key.
+async function gsrun(cl){
+	const gsapi = google.sheets({version:'v4', auth: cl });
+	const opt = {
+		spreadsheetid:'1iPU6hnVEWl2TCNVz2Zk3Fyq331HmzZlpM6o-9bBtsOo',
+		range: 'Data!A1:F60'
+	};
+	
+	temp = await gsapi.spreadsheets.values.get(opt);
+	console.log(temp);
+}
 
-      var CLIENT_ID = '1021725875690-r08u3n6aclvuog72ud8fqrn1rvm0f6hh.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
 
-      // TODO: Authorize using one of the following scopes:
-      //   'https://www.googleapis.com/auth/drive'
-      //   'https://www.googleapis.com/auth/drive.file'
-      //   'https://www.googleapis.com/auth/drive.readonly'
-      //   'https://www.googleapis.com/auth/spreadsheets'
-      //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-      var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
-	    
-	    /*
-	    gapi.client.init({
-        'apiKey': '3379d05a4cacb1890cc85fef4a9ca4dce676db7f',
-        'clientId': '100855812677045796937',
-        'scope': SCOPE,
-        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-      }).then(function() {
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      });*/
 
-      gapi.client.init({
-        'apiKey': API_KEY,
-        'clientId': CLIENT_ID,
-        'scope': SCOPE,
-        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-      }).then(function() {
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      });
-      
-    }
-
-    function handleClientLoad() {
-      gapi.load('client:auth2', initClient);
-    }
-
-    function updateSignInStatus(isSignedIn) {
-      if (isSignedIn) {
-        makeApiCall();
-      }
-    }
-
-    function handleSignInClick(event) {
-      gapi.auth2.getAuthInstance().signIn();
-    }
-
-    function handleSignOutClick(event) {
-      gapi.auth2.getAuthInstance().signOut();
-    }
-    
 
 
 
